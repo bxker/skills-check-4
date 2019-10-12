aa87549f185a433d2c6aed0c2b4b04062f8b701a
bxker <jake.luke.baker@gmail.com>
2019-10-12 14:39:52 -0500

    finished auth controller and all endpoints work as expected

---
+++ server/controllers/authController.js
@@ -0,0 +1,59 @@
+const bcrypt = require('bcryptjs')
+
+const getUser = async (req, res) => {
+    res.status(200).json(req.session.user)
+}
+const register = async (req, res) => {
+    const db = req.app.get('db');
+    const {username, password} = req.body;
+    
+    const foundUser = await db.auth.checkForUsername(username);
+
+    if(foundUser[0]){
+        res.status(409).json('Username taken')
+    }else{
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+
+        const newUser = await db.auth.registerUser(username, hash);
+        req.session.user = {
+            username: newUser[0].username,
+            profile_pic: newUser[0].profile_pic
+        };
+        res.status(200).json(req.session.user);
+    }
+}
+const login = async (req, res) => {
+    const db = req.app.get('db');
+    const {username, password} = req.body;
+    console.log(username)
+
+    const foundUser = await db.auth.checkForUsername(username);
+
+    if(!foundUser[0]){
+        res.status(403).json('Username or Password is incorrect 1');
+    }else{
+        const isAuthenticated = bcrypt.compareSync(password, foundUser[0].hash)
+
+        if(!isAuthenticated){
+            res.status(403).json('Username or Password is incorrect 2');
+        }else{
+            req.session.user = {
+                username: foundUser[0].username,
+                profile_pic: foundUser[0].profile_pic 
+            }
+            res.status(200).json(req.session.user)
+        }
+    }
+}
+const logout = async (req, res) => {
+    req.session.destroy();
+    res.status(200).json('logged out successfully')
+}
+
+module.exports = {
+    getUser,
+    register,
+    login,
+    logout
+}
---
+++ server/index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const massive = require('massive');
+const session = require('express-session');
+
+//controllers
+const {getUser, register, login, logout} = require('./controllers/authController');
+
+//dotenv
+const {SESSION_SECRET, CONNECTION_STRING, SERVER_PORT} = process.env;
+
+//middleware
+app.use(express.json());
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000*60*60*24*7
+    }
+}));
+
+//database connection
+massive(CONNECTION_STRING)
+.then(db => {
+    app.set('db', db);
+    console.log('Database Connected :D')
+});
+
+//auth endpoints
+app.get('/auth/user', getUser);
+app.post('/auth/register', register);
+app.post('/auth/login', login);
+app.post('/auth/logout', logout);
+
+
+//api endpoints
+
+//server connection
+app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}`));
