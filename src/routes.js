8786efbfb20e38791f271652235fd410531b54f0
bxker <jake.luke.baker@gmail.com>
2019-10-12 18:29:49 -0500

    added alot of front end and redux

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Login from '../src/Components/Login/Login';
+import Dashboard from '../src/Components/Dashboard/Dashboard';
+import Post from '../src/Components/Post/Post';
+import AddPost from '../src/Components/AddPost/AddPost';
+
+export default (
+    <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/post/:id" component={Post} />
+        <Route path="/post/add" component={AddPost}/>
+        <Route render={() => {
+            return <h1>404 Not Found</h1>
+        }}/>
+    </Switch>
+)
